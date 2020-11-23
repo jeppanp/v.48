250548fdcf12ef8bdaae688b7ad7f13b87aa4818
Jesper Persson <mrjesperpersson@hotmail.se>
2020-11-23 14:48:13 +0100

    Added menu, in an own method

--- v.48/Program.cs
+++ v.48/Program.cs
@@ -5,6 +5,7 @@ namespace v._48
 {
     class Program
     {
+        static bool Success; 
         static void Main(string[] args)
         {
             bool keepGoing = true;
@@ -14,8 +15,25 @@ namespace v._48
             string userAnswer = Console.ReadLine();
             while(keepGoing)
             IsPinCorrect(userAnswer);
- 
+
+            while(keepGoing)
+            {
+                Menu();
+
+            }
         }
+
+         static int Menu()
+        {
+            Console.WriteLine("What do you wanna do?");
+            Console.WriteLine("1. Deposit money");
+            Console.WriteLine("2. Whitdraw money");
+            Console.WriteLine("3. Check Balance");
+            Console.WriteLine("4. Collect intresst");
+            Success = Int32.TryParse(Console.ReadLine(), out int choice);
+            return choice;
+        }
+
         static void IsPinCorrect(string a)
         {
            
