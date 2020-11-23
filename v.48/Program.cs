fbffeab95d327fcf3f9a025acb9247b6859ecb7f
Jesper Persson <mrjesperpersson@hotmail.se>
2020-11-23 15:59:35 +0100

    Balance-method added.

--- v.48/Program.cs
+++ v.48/Program.cs
@@ -5,25 +5,62 @@ namespace v._48
 {
     class Program
     {
-        static bool Success; 
+        static bool Success;
         static void Main(string[] args)
         {
+            List<double> accountHistory = new List<double>();
             bool keepGoing = true;
             
-            Console.WriteLine("Welcome to National Security Bank");
-            Console.WriteLine("Enter yout pincode: ");
-            string userAnswer = Console.ReadLine();
-            while(keepGoing)
-            IsPinCorrect(userAnswer);
-
-            while(keepGoing)
+            
+            do
             {
-                Menu();
+                Console.WriteLine("Welcome to National Security Bank");
+                IsPinCorrect();
+                int choice = Menu();
+                switch(choice)
+                {
+                    case 1:
+                        accountHistory.Add(Deposit());
+                        break;
+                    //case 2:
+                    //    Withdraw();
+                    //    break;
 
-            }
+                    case 3:
+
+                        Console.WriteLine(Balance(accountHistory)); 
+                        break;
+
+                        //case 4:
+                        //    CollectInteresst();
+                        //    break;
+
+                }
+              
+
+            } while (keepGoing);
+
+
         }
 
-         static int Menu()
+        static double Balance(List<double> accountHistory)
+        {
+            double balance = 0;
+            foreach (double element in accountHistory)
+            {
+                balance += element;
+            }
+            return balance;
+        }
+
+        static double Deposit()
+        {
+            Console.WriteLine("How much do you wanna deposit?");
+            Success = double.TryParse(Console.ReadLine(), out double result);
+            return result;
+        }
+
+        static int Menu()
         {
             Console.WriteLine("What do you wanna do?");
             Console.WriteLine("1. Deposit money");
@@ -34,16 +71,39 @@ namespace v._48
             return choice;
         }
 
-        static void IsPinCorrect(string a)
+        static void IsPinCorrect()
         {
-           
-                 bool sucess = Int32.TryParse(a, out int pin);
-            
-                if (sucess && a.Length == 4 || a.Length == 6)
+            int invaildPin = 0;
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                
+                Console.WriteLine("Enter yout pincode: ");
+                string userAnswer = Console.ReadLine();
+
+                bool success = Int32.TryParse(userAnswer, out int pin);
+
+                if (success && userAnswer.Length == 4 || success && userAnswer.Length == 6)
+                {
                     Console.WriteLine("Valid pin, Welcome human");
-                else
+                    keepGoing = false;
+                }
+                else if (invaildPin == 2)
+                {
+                    Console.WriteLine("Your account is blocked. Entered wrong pin 3 times. Please contact your bank.");
+                    keepGoing = false;
+                }
+                else if (success! || userAnswer.Length != 4 || userAnswer.Length != 6)
+                { 
                     Console.WriteLine("Invalid pin");
-            
+                    invaildPin++;
+                }
+
+
+
+
+
+            }
         }
     }
 }
